41639a07f6b7717892c8c0166562661428ef153b
Daniel Teixeira <dant25@gmail.com>
2018-05-01 14:24:22 -0300

    Várias melhorias feias. Agora está treinando e testando.

--- Classifica_reclamacoes2012.py
+++ Classifica_reclamacoes2012.py
@@ -1,30 +1,34 @@
 import pandas as pd
 data_frames = pd.read_csv('reclamacoes2012.csv')
 
-X_df = data_frames[['Regiao','Tipo','SexoConsumidor']]
 
-Y_df = data_frames[['Atendida']]
+X_df = data_frames[['Regiao','Tipo','SexoConsumidor']]
+Y_df = data_frames.Atendida
+
+Y = list()
+
+for i, value in enumerate(Y_df):
+    if ( Y_df[i] == 'S'):
+        Y.append(1)
+    else:
+        Y.append(0)
+
+#TODO - TIRAR O Xdummies_df e fazer a mesma coisa q fiz
 
 Xdummies_df = pd.get_dummies(X_df)
 
-Ydummies_df = pd.get_dummies(Y_df)
+#Ydummies_df = pd.get_dummies(Y_df)
 
 X =  Xdummies_df.values         #Devolve os Dummies em Arrays
-
-Y =  Y_df.values
 
 
 
 #codigo para efetuar o treino e teste
+from sklearn.model_selection import train_test_split
+treino_dados, teste_dados, treino_marcacoes, teste_marcacoes = train_test_split(X, Y, test_size=0.1)
 
-tamanho_de_treino = int(0.9*len(Y))
-tamanho_de_teste = len(Y)-tamanho_de_treino
-
-treino_dados     = X[:tamanho_de_treino]
-treino_marcacoes = Y[:tamanho_de_treino]
-
-teste_dados     =   X[-tamanho_de_teste:]
-teste_marcacoes =   Y[-tamanho_de_teste:]
+print( treino_dados )
+print( treino_marcacoes )
 
 
 
@@ -33,15 +37,7 @@ from sklearn.naive_bayes import  MultinomialNB
 modelo = MultinomialNB()
 modelo.fit(treino_dados,treino_marcacoes)
 
-resultado = modelo.predict(teste_dados)
-diferencia =  resultado - teste_marcacoes
+#resultado = modelo.predict(teste_dados)
 
-acertos = [d for d in diferencia if d == 0]
-total_de_acertos = len(acertos)
-total_de_elementos = len(teste_dados)
-
-taxa_de_acerto = 100.0 * total_de_acertos/total_de_elementos
-
-
-print(taxa_de_acerto)
-print(total_de_elementos)
+valor = modelo.score(teste_dados, teste_marcacoes)
+print( "Precisão: ", valor )
