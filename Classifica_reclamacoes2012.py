5991f1d42cb7a42cb1fbb8a83026def57b3e3033
Daniel Teixeira <dant25@gmail.com>
2018-08-24 15:37:34 -0300

    Adicionado outras formas de aprendizado.

--- Classifica_reclamacoes2012.py
+++ Classifica_reclamacoes2012.py
@@ -1,3 +1,4 @@
+from time import time
 import pandas as pd
 data_frames = pd.read_csv('reclamacoes2012.csv')
 
@@ -27,17 +28,35 @@ X =  Xdummies_df.values         #Devolve os Dummies em Arrays
 from sklearn.model_selection import train_test_split
 treino_dados, teste_dados, treino_marcacoes, teste_marcacoes = train_test_split(X, Y, test_size=0.1)
 
-print( treino_dados )
-print( treino_marcacoes )
+#print( treino_dados )
+#print( treino_marcacoes )
 
 
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import LinearSVC
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import  MultinomialNB
 
-modelo = MultinomialNB()
-modelo.fit(treino_dados,treino_marcacoes)
+clf_dict = {'log reg': LogisticRegression(),
+            'naive bayes': GaussianNB(),
+            'random forest': RandomForestClassifier(n_estimators=100),
+            'knn': KNeighborsClassifier(),
+            'linear svc': LinearSVC(),
+            'ada boost': AdaBoostClassifier(n_estimators=100),
+            'gradient boosting': GradientBoostingClassifier(n_estimators=100),
+            'CART': DecisionTreeClassifier(),
+            'Multinomial Naive Bayes': MultinomialNB()}
 
-#resultado = modelo.predict(teste_dados)
+for name, clf in clf_dict.items():
+    t0 = time()
+    model = clf.fit(treino_dados, treino_marcacoes)
+    pred = model.predict(teste_dados)
+    model.score(teste_dados, teste_marcacoes)
+    print('Precisão {}:'.format(name), model.score(teste_dados, teste_marcacoes))
+    print("Tempo gasto:", round(time() - t0, 3), "s")
 
-valor = modelo.score(teste_dados, teste_marcacoes)
-print( "Precisão: ", valor )
+print("Fim da execução!")
